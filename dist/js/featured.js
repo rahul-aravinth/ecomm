62d8deb4524a1699c32156145880b9e3426f6d78
Rahulthangaraj <rahulthangaraj@hakunamatata.in>
2025-04-23 16:57:31 +0530

    dist removed from gitignore

---
+++ dist/js/featured.js
@@ -0,0 +1,1 @@
+const featuredItemButtons=document.querySelectorAll(".js-featuredItemButton");featuredItemButtons.forEach((t=>{t.addEventListener("click",(()=>{const e=document.getElementById("products"),o=t.getAttribute("data-name"),n=e.querySelector(`.js-productCategory[data-name="${o}"]`);n&&(n.dispatchEvent(new Event("click")),e.scrollIntoView({behavior:"smooth"}))}))}));
